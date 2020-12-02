836ff06c15f2504517e81be66592ff0579582118
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-02 09:05:04 +0530

    [Prudhvi] add . adding multiple contacts

--- AddressBookRestApiTest/AddressBookTest.cs
+++ AddressBookRestApiTest/AddressBookTest.cs
@@ -2,6 +2,7 @@ using AddressBookRestAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MySql.Data.MySqlClient.Memcached;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Collections.Generic;
 using System.Net;
@@ -29,6 +30,35 @@ namespace AddressBookRestApiTest
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<AddressBookModel> dataResponse = JsonConvert.DeserializeObject<List<AddressBookModel>>(response.Content);
             Assert.AreEqual(4, dataResponse.Count);
+        }
+        [TestMethod]
+        public void GivenMultipleContactEntries_UsingPostOperation_ShouldReturnAddedContacts()
+        {
+            List<AddressBookModel> contactList = new List<AddressBookModel>();
+            contactList.Add(new AddressBookModel { firstName = "RAJU",secondName="SHAH", address = "RN nagar", city="tiruvallur",state="tamilnadu",zip=90876,phoneNumber = 999888999888, emailid = "raju@gmail.com", contactType = "Professional" , addressBookName="local"});
+            contactList.Add(new AddressBookModel { firstName = "Somesh", secondName = "SHAH", address = "hope farm", city = "Bangalore", state = "Karnataka", zip = 90876, phoneNumber = 999888999888, emailid = "somesh@gmail.com", contactType = "Friends", addressBookName = "local" });
+            foreach (AddressBookModel contact in contactList)
+            {
+                RestRequest request = new RestRequest("/addressBook", Method.POST);
+                JObject jObject = new JObject();
+                jObject.Add("FirstName", contact.firstName);
+                jObject.Add("secodName", contact.secondName);
+                jObject.Add("address", contact.address);
+                jObject.Add("city", contact.city);
+                jObject.Add("state", contact.state);
+                jObject.Add("zip", contact.zip);
+                jObject.Add("phonenumber", contact.phoneNumber);
+                jObject.Add("emailid", contact.emailid);
+                jObject.Add("contactType", contact.contactType);
+                jObject.Add("addressBookName", contact.addressBookName);
+                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+                IRestResponse response = restClient.Execute(request);
+                //Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                //derserializing object for assert and checking test case
+                AddressBookModel dataResponse = JsonConvert.DeserializeObject<AddressBookModel>(response.Content);
+                Assert.AreEqual(contact.firstName, dataResponse.firstName);
+            }
         }
     }
 }
